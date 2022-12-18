db4d4c3ead85e600aa1683697c06448e9dc06f98
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2022-12-18 15:21:21 +0300

    fix

--- Değer Değiştirme.py
+++ Değer Değiştirme.py
@@ -3,4 +3,8 @@ print("Değer Değiştirci")
 a =int(input("sayı 1:"))
 b =int(input("sayı 2:"))
 
-print("sayı 1: {} \nsayı 2: {}\n".format(b,a))
+temp = a
+a = b
+b = temp
+
+print("sayı 1: {} \nsayı 2: {}\n".format(a,b))
